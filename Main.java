ea7fee5241272c2d11a8a4737f6872e99951cd88
Lucas Gabriel <160306251+ksslucas@users.noreply.github.com>
2024-07-25 00:17:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 30;
+
+        int result = numberTwo + numberOne;
+
+        String nome = "Lucas";
+        int age = 17;
+        String school = "IEMA";
+        String sex = "Masculino";
+
+        System.out.println("Meu nome é "+ nome +
+                ", tenho "+ age +" de idade, "+ "sou do sexo "+ sex +" e estudo no instituto "+
+                school+ " e o resultado dessa conta é: "+ result);
+    }
+}
---
+++ Somar.java
@@ -0,0 +1,10 @@
+public class Somar {
+        public static void main(String[] args) {
+            float numberOne = (int) 2500.000F;
+            long numberTwo = (int) 10F;
+
+            float result =  numberOne / numberTwo ;
+
+            System.out.println(result);
+        }
+}
